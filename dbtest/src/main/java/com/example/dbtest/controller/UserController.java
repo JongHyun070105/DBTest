356db5ba238242bf62ccbcb397931f07009676f6
JongHyun070105 <bobi09361@gmail.com>
2025-01-07 10:07:55 +0900

    "master"

---
+++ dbtest/src/main/java/com/example/dbtest/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.dbtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class UserController {
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+}
