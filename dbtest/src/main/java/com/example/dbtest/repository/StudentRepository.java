d3e24255c864a81570cf480d6f92f8f129dbb3b5
JongHyun070105 <bobi09361@gmail.com>
2025-01-07 11:41:49 +0900

    "master"

--- dbtest/src/main/java/com/example/dbtest/repository/StudentRepository.java
+++ dbtest/src/main/java/com/example/dbtest/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.example.dbtest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.dbtest.model.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
-    Student findByName(String name);
+    List<Student> findByName(String name);
 }
